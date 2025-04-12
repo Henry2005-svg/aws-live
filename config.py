64b8bc8bfa2701214e0ceb8a94fec7fdcc618078
Henry2005-svg <henrytxw2005@gmail.com>
2025-04-12 22:27:52 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ctujx4x4dhpl.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "henrytan-employee"
+customregion = "us-east-1"
+
